8133f490d93bcbd7450fc67b1e40532bb473fdc8
Gabriel Staples <ercaguy@gmail.com>
2020-11-01 23:03:24 -0800

    Follow the basic "Remote theme method" instructions
    ...here: https://github.com/mmistakes/minimal-mistakes#remote-theme-method
    
    Also add my run_build_site.sh and run_upgrade_site.sh scripts.

---
+++ run_upgrade_site.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# See the "Gemfile" here.
+# See also other links below.
+
+# First, install & update:
+# See: https://github.com/prose/starter/issues/44#issuecomment-562822698
+
+# gem install github-pages
+# bundle update
+# bundle update jekyll
+# # installs gems in your Gemfile using the Bundler gem; see here:
+# # https://jekyllrb.com/docs/ruby-101/
+# # Additional reading: https://jekyllrb.com/tutorials/using-jekyll-with-bundler/
+bundle
+bundle install
+bundle update
+
+# Then run the following periodically to upgrade and ensure we have the latest. See also:
+# https://docs.github.com/en/free-pro-team@latest/github/working-with-github-pages/testing-your-github-pages-site-locally-with-jekyll#updating-the-github-pages-gem
+bundle update github-pages
+
+# Then you can test your site with:
+# ./run_build_site.sh
+# # OR:
+# bundle exec jekyll serve
